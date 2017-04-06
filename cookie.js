42cb5e834ddb776b707afb4677491667425201e7
Joel Varughese <jtvarughese@gmail.com>
2017-04-05 20:09:10 -0400

    buttons tally the count and clear button added

--- cookie.js
+++ cookie.js
@@ -1,18 +1,44 @@
-document.cookie = "hello=test"
+var sugar = document.querySelector("#sugarButton");
+var chocolate = document.querySelector("#chocolateButton");
+var lemon = document.querySelector("#lemonButton");
+var clear = document.querySelector("#clearButton");
 
+var sugarValue = 0;
+var chocolateValue = 0;
+var lemonValue = 0;
 
-// if(Cookies.get("sugar")){
-//   alert("Nomnomnom")
-// } else {
-//   Cookies.set("name", "Joel")
-//   alert("Buy more cookies")
-// }
-var buttonElement = document.querySelector("button")
+Cookies.set("sugar", sugarValue);
+Cookies.set("chocolate", chocolateValue);
+Cookies.set("lemon", lemonValue);
 
-var sugarCookie = document.getElementById('sugar')
-var chocolateCookie = document.getElementById('chocolate')
-var lemonCookie = document.getElementById('lemon')
+sugar.addEventListener("click", function(){
+  event.preventDefault();
+    alert("You added a sugar cookie");
+    sugarValue += 1;
+    Cookies.set("sugar", sugarValue);
+    sugar.innerHTML = sugarValue;
+})
 
-buttonElement.addEventListener("click", function(){
-  alert("Thank you buying a cookie!")
+chocolate.addEventListener("click", function(){
+  event.preventDefault()
+    alert("You added a chocolate cookie")
+    chocolateValue += 1
+    Cookies.set("chocolate", chocolateValue)
+    chocolate.innerHTML = chocolateValue
+})
+
+lemon.addEventListener("click", function(){
+  event.preventDefault();
+    alert("You added a lemon cookie");
+    lemonValue += 1;
+    Cookies.set("lemon", lemonValue);
+    lemon.innerHTML = lemonValue;
+})
+
+clear.addEventListener("click", function(){
+  event.preventDefault();
+    // alert("Are you sure that you want to do that?");
+    sugar.innerHTML = sugarValue = 0;
+    chocolate.innerHTML = chocolateValue = 0;
+    lemon.innerHTML = lemonValue = 0;
 })
